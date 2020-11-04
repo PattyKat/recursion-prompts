9d217edb5f6ee705588d31977d0a99fe446e9056
Patrice Cottman <patrice.cottman@gmail.com>
2020-11-04 00:55:02 +0000

    Completed Factorial, sum and almost done debugging arraySum

--- src/recursion.js
+++ src/recursion.js
@@ -7,16 +7,47 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if(n === 0){
+    return 1;
+  }else if (n < 0) {
+    return null;
+  } else {
+    return n * factorial( n - 1) ;
+  }
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0){
+    return 0;
+  }else if(array.length === 1){
+    return array[0];
+  }else{
+    var arrayCopy = array.slice(0);
+    var popVar = arrayCopy.pop();
+    arrayCopy[arrayCopy.length - 1] = arrayCopy[arrayCopy.length - 1] + popVar;
+    return sum(arrayCopy);
+    ;
+  }
+
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (array.length === 0){
+    return 0;
+  }
+  if(array.length === 1){
+    return array[0];
+  }
+    var flatCopy = array.flat(Infinity);
+    var popVar = flatCopy.pop();
+    flatCopy[flatCopy.length -1] = flatCopy[flatCopy.length - 1] + popVar;
+    return arraySum(flatCopy);
+
+
 };
 
 // 4. Check if a number is even.
